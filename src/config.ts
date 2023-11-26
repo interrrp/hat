3cf09a1deb28f8b50c96adbf5f69cf93720497d7
interrrp <int@addictedto.coffee>
2023-11-26 08:14:21 +0800

    Make configuration loading smarter

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,5 @@
 import { parse as parseYAML } from "std/yaml/mod.ts";
+import { info } from "./logger.ts";
 
 export type Config = {
   host: string;
@@ -24,4 +25,13 @@ async function loadConfigFromYAMLFile(path: string): Promise<Config> {
   } as Config;
 }
 
-export const config = await loadConfigFromYAMLFile("./hat.yaml");
+export const config = defaultConfig;
+try {
+  const configPath = Deno.env.get("HAT_CONFIG_PATH") || "./config.yml";
+  const loadedConfig = await loadConfigFromYAMLFile(configPath);
+  Object.assign(config, loadedConfig);
+} catch (error) {
+  if (error instanceof Deno.errors.NotFound) {
+    info("No config file found, using default config");
+  }
+}
