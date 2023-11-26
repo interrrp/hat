2873417921f6f3dc3323083a8053950018b80b17
interrrp <int@addictedto.coffee>
2023-11-26 08:20:36 +0800

    Replace `Deno.serve` logging with custom logger

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,11 @@
 import { config } from "./config.ts";
+import { info } from "./logger.ts";
 import { handleRequest } from "./server.ts";
 
-Deno.serve({ hostname: config.host, port: config.port }, handleRequest);
+Deno.serve({
+  hostname: config.host,
+  port: config.port,
+  onListen: () => {
+    info(`Listening on http://${config.host}:${config.port}`);
+  },
+}, handleRequest);
